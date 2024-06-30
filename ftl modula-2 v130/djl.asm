928bb9ac3b597b65cbdc8db446e3e96d67622fcf
davidly <davidly_lee@hotmail.com>
2024-06-30 06:23:04 -0700

    make output work on cp/m implementations that don't preserve hl on bdos calls

--- ftl modula-2 v130/djl.asm
+++ ftl modula-2 v130/djl.asm
@@ -1,8 +1,10 @@
         label           ; Write
         pop     hl      ; return address
         pop     bc      ; character to write
+        push    hl      ; save it again
         ld      e, c    ; character to write
         ld      c, 2    ; console output
         call    0005
+        pop     hl      ; return address
         jp      (hl)
 
