8209a35b625241debe396a39d2f50c2f561a694a
David Lee <davidly_lee@hotmail.com>
2024-07-03 08:13:43 -0700

    linux build

---
+++ Whitesmith C v2/m.sh
@@ -0,0 +1,18 @@
+str=${1^^}
+
+rm $str.COM 2>/dev/null
+rm $str.S 2>/dev/null
+rm $str.O 2>/dev/null
+rm $str.TM1 2>/dev/null
+rm $str.TM2 2>/dev/null
+
+ntvcm PP -x -o $str.TM1 $str.C
+ntvcm P1 -b0 -n8 -o $str.TM2 $str.TM1
+ntvcm P2 -o $str.S $str.TM2
+ntvcm AN -o $str.O $str.S
+ntvcm LD80 -o $str.COM CHDR.R $str.O CLIB.A MLIB.A
+
+rm $str.S 2>/dev/null
+rm $str.O 2>/dev/null
+rm $str.TM1 2>/dev/null
+rm $str.TM2 2>/dev/null
