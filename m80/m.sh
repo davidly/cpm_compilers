196d8ef55b395a1e8fda04bc1899d6e1b79b47e2
David Lee <davidly_lee@hotmail.com>
2024-07-03 09:44:24 -0700

    linux build

---
+++ m80/m.sh
@@ -0,0 +1,12 @@
+str=${1^^}
+
+rm $str.REL 2>/dev/null
+rm $str.PRN 2>/dev/null
+rm $str.COM 2>/dev/null
+
+ntvcm M80 =$str.MAC /X /O /L /Z
+ntvcm L80 $str,$str/N/E
+
+rm $str.REL 2>/dev/null
+rm $str.PRN 2>/dev/null
+
