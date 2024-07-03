d0d4beecfce586e6a1b932dd45323ed1dc1fdfc0
David Lee <davidly_lee@hotmail.com>
2024-07-03 08:00:21 -0700

    linux build

---
+++ SuperSoft C v1125/m.sh
@@ -0,0 +1,11 @@
+str=${1^^}
+
+ntvcm CC $str.C
+ntvcm C2 $str.COD
+ntvcm M80 =$str.ASM
+ntvcm L80 $str,FORMATIO,CRUNT2,FUNC,STDIO,ALLOC,C2RT,$str/N/E:CCSTAR
+
+rm $str.COD 2>/dev/null
+rm $str.ASM 2>/dev/null
+rm $str.REL 2>/dev/null
+
