0fc78e49e26d9db89805b8be1b0d87e61c7616bd
David Lee <davidly_lee@hotmail.com>
2024-07-19 15:36:25 -0700

    linux build + windows build cleanup

--- InterSystems Pascal-Z v4/m.bat
+++ InterSystems Pascal-Z v4/m.bat
@@ -3,7 +3,7 @@ ntvcm Pascal48 %1
 rem pasopt requires some uninitialized RAM to be non-zero to work. It depends on some other
 rem app having run to initialize that memory. It's not a single byte -- it's bytes per parse
 rem record spread over the array of records. NTVCM has a workaround.
-ntvcm -t pasopt %1.SRC
+ntvcm pasopt %1.SRC
 
 ntvcm asmbl main,%1/rel
 
--- InterSystems Pascal-Z v4/m.sh
+++ InterSystems Pascal-Z v4/m.sh
@@ -1,20 +1,24 @@
 str=$(tr '[a-z]' '[A-Z]' <<< $1)
 
 rm $str.COM 2>/dev/null
+rm $str.ORG 2>/dev/null
 rm $str.LST 2>/dev/null
 rm $str.REL 2>/dev/null
 rm $str.SRC 2>/dev/null
 
 ntvcm PASCAL48 $str
 
-# pasopt just goes into an infinite loop writing to the output file
-# ntvcm pasopt %1.SRC
+# pasopt requires some uninitialized RAM to be non-zero to work. It depends on some other
+# app having run to initialize that memory. It's not a single byte -- it's bytes per parse
+# record spread over the array of records. NTVCM has a workaround.
+ntvcm PASOPT $str.SRC
 
 ntvcm ASMBL main,$str/rel
 
 # /e means link. /g means link and run
 ntvcm LINK $str /n:$str /e
 
+rm $str.ORG 2>/dev/null
 rm $str.LST 2>/dev/null
 rm $str.REL 2>/dev/null
 rm $str.SRC 2>/dev/null
