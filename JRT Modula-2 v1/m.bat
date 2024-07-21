00a30d6b8522a8e3239b3959f4fa850c1300a776
davidly <davidly_lee@hotmail.com>
2024-07-21 10:37:45 -0700

    JRT Modula-2 v1

---
+++ JRT Modula-2 v1/m.bat
@@ -0,0 +1,11 @@
+rem I can't find documentation and I can't figure out to generate an .int file. So this is a workaround:
+
+copy modula.lib %1.lib
+
+rem compile the file and put the code in %1.lib along with everything else in modula.lib
+ntvcm modula %1=%1
+
+rem execute the library (dot) app name, which isn't a filename but the syntax for running an app in a library
+ntvcm -p MEX.COM %1.%1
+
+
