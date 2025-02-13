1e5cd4391c095169bdd3fca674391814cf363114
davidly <davidly_lee@hotmail.com>
2025-02-12 20:16:24 -0800

    add Sorcim Pascal/M (two versions)

---
+++ Sorcim Pascal M 1981/m.bat
@@ -0,0 +1,7 @@
+ntvcm -c prun pascal %1.pas, L=%1.lst
+
+ntvcm -p -c prun %1.pco
+
+ntvcm -p -c prunz80 %1.pco
+
+
---
+++ Sorcim Pascal M v12.08/m.bat
@@ -0,0 +1,4 @@
+ntvcm -c prun pascal %1.pas,l=console:
+
+ntvcm -p -c prun %1.pco
+
