84fdb7b0f6ed94295727e6fd8f13804654ae3cdd
davidly <davidly_lee@hotmail.com>
2025-02-20 14:03:03 -0800

    update benchmarks; only ttt is improved

--- Whitesmith C v2/E.C
+++ Whitesmith C v2/E.C
@@ -1,22 +1,16 @@
+/*
+    this is overall 25% faster for Whitesmith, but it's not the benchmark.
+    #define FAST_WHITESMITH
+*/
+
 #define DIGITS_TO_FIND 200 /*9009;*/
 
-int strlen( p ) char * p;
-{
-    int l;
-    l = 0;
-
-    while ( 0 != p[l] )
-        l++;
-
-    return l;
-}
-
-int reverse( p ) char * p;
+int reverse( p, len ) char * p; int len;
 {
     int r, l;
     char t;
 
-    r = strlen( p );
+    r = len;
     if ( 0 == r )
         return 0;
 
@@ -34,61 +28,78 @@ int reverse( p ) char * p;
     return 0;
 }
 
-int itoa( p, i ) char * p; int i;
+int utoa( p, i ) char * p; int i;
 {
-    int x, len;
-
-    if ( 0 == i )
-    {
-        p[0] = '0';
-        p[1] = 0;
-        return 1;
-    }
+    int digit, len;
 
     len = 0;
-    while ( 0 != i )
+    do
     {
-        x = i % 10;
-        p[ len ] = '0' + x;
+        if ( i >= 10 )
+        {
+            digit = i % 10;
+            i /= 10;
+        }
+        else
+        {
+            digit = i;
+            i = 0;
+        }
+
+        p[ len ] = '0' + digit;
         len++;
-        i /= 10;
-    }
+    } while ( 0 != i );
 
     p[ len ] = 0;
 
-    reverse( p );
+    if ( len > 1 )
+        reverse( p, len );
+
     return len;
 }
 
-int main() {
-  int N;
-  int x;
-  int n;
-  int l;
-  char ac[ 10 ];
-  int a[ DIGITS_TO_FIND ];
 
-  N = DIGITS_TO_FIND;
-  x = 0;
+int main()
+{
+#ifdef FAST_WHITESMITH
+    static int N, x, n, l, d, t;
+    static char ac[ 10 ];
+    static int a[ DIGITS_TO_FIND ];
+#else
+    int N, x, n;
+    char ac[ 10 ];
+    int a[ DIGITS_TO_FIND ];
+#endif
 
-  for (n = N - 1; n > 0; --n) {
-      a[n] = 1;
-  }
+    N = DIGITS_TO_FIND;
+    x = 0;
 
-  a[1] = 2;
-  a[0] = 0;
-  while (N > 9) {
-      n = N--;
-      while (--n) {
-          a[n] = x % n;
+    for ( n = N - 1; n > 0; --n )
+      a[ n ] = 1;
 
-          x = 10 * a[n-1] + x/n;
-      }
-      l = itoa( ac, x );
-      write( 0, ac, l );
-  }
+    a[ 1 ] = 2;
+    a[ 0 ] = 0;
 
-  write( 0, "\ndone\n", 6 );
+    while ( N > 9 )
+    {
+        n = N--;
+        while ( --n )
+        {
+#ifdef FAST_WHITESMITH
+            d = x / n;
+            a[ n ] = x - ( d * n );
+            t = a[ n - 1 ];
+            x = ( t << 3 ) + ( t << 1 ) + d;
+#else
+            a[ n ] = x % n;
+            x = 10 * a[ n - 1 ] + x / n;
+#endif
+        }
 
-  return 0;
+        n = utoa( ac, x );
+        write( 0, ac, n );
+    }
+
+    write( 0, "\ndone\n", 6 );
+    return 0;
 }
